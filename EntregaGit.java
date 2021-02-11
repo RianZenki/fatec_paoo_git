19c69690e321343519e1ee33c0701534ed409820
Rian Nacazato <rian.zenki@gmail.com>
2021-02-11 15:37:46 -0300

    Exercicio finalizado

--- EntregaGit.java
+++ EntregaGit.java
@@ -1,5 +1,11 @@
+import javax.swimg.JOptionPane;
+
 public class EntregaGIt {
     public static void main (String args[]) {
+        String nome = JOptionPane.showInputDialog("Qual é o seu nome?");
+        JOptionPane.showMessageDialog(null, "Bem vindo " + nome);
         System.out.println("Entregando tarefas com o Git na linha de comando");
+        //teste
+        // outro teste
     }
 }
